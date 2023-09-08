3582d31713beae79683f97cd47f8b027c143bbff
olyadev <yusupowa9494@mail.ru>
2023-09-08 14:45:59 +0300

    #16 - Entitilerimmizn Oluşturulması

---
+++ NLayer.Core/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    //Entityler içersindeki böyle farklı class'lara farklı Entit'lere referance verdiğimiz propertilere biz --> "Navigation Property" diyoruz.(Neden Navigation? - Çünkü : Category'den --> Product'lara gidebiliyorum!!!! (yani Category'ye bağlı tüm Product'ları çekebilirim....))
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //Categoryde birden fazla Product olabilir....
+        public ICollection<Product> Products { get; set; }
+    }
+}
