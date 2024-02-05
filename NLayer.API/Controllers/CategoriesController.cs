0eb178414fca60febd0b9f6686fe81d38869d259
user <user@DESKTOP-9E5P6SH>
2024-02-05 08:01:38 +0300

    NLayer.Api FluentValidation Library-2
    Filter işlemleir yapıldı.
    (Bir şeey global ise PRogram.cs dosyasına yazmamız lazım)

--- NLayer.API/Controllers/CategoriesController.cs
+++ NLayer.API/Controllers/CategoriesController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.API.Filters;
 using NLayer.Core.Services;
 
 namespace NLayer.API.Controllers
 {
     public class CategoriesController : CustumBaseController
     {
-
         private readonly ICategoryService _categoryService;
 
         public CategoriesController(ICategoryService categoryService)
--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,5 +1,7 @@
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NLayer.API.Filters;
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
@@ -19,7 +21,19 @@ internal class Program
 
         // Add services to the container.
 
-        builder.Services.AddControllers().AddFluentValidation(x=>x.RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+        builder.Services.AddControllers(options =>
+        options.Filters
+            .Add(new ValidateFilterAttribute()))
+            .AddFluentValidation(x=>x
+            .RegisterValidatorsFromAssemblyContaining<ProductDtoValidator>());
+
+
+        //Default olarak bu Filter aktiv oldugu için bunu buraya yazmamýz lazým ki bizim yazdýðýmýz Validatimiz çalýþsýn (Ama MVC tarafýnda bunun aktin olma tarafý yok)
+        builder.Services.Configure<ApiBehaviorOptions>(options =>
+        {
+            options.SuppressModelStateInvalidFilter = true; 
+        });
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
